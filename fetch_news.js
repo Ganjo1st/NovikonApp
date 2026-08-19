eabddc1e497b9e8dcbf7eaee525f0d60b5e92cb0
Ganjo1st <ganjo1986@mail.ru>
2026-08-20 00:01:58 +0700

    Исправлен конструктор TelegramBot (добавлен .default)

--- fetch_news.js
+++ fetch_news.js
@@ -5,8 +5,7 @@ const path = require('path');
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 
-const bot = new TelegramBot(BOT_TOKEN, { polling: false });
-
+const bot = new TelegramBot.default(BOT_TOKEN, { polling: false });
 async function fetchNews() {
     try {
         if (!BOT_TOKEN || !CHANNEL_ID) {
