0b8a83fd481935e4aca615b667b642d81e95a212
Ganjo1st <ganjo1986@mail.ru>
2026-08-20 21:44:17 +0700

    Исправлен 404, добавлен historyApiFallback

--- website/vite.config.js
+++ website/vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/NovikonApp/',
+  server: {
+    // Эта строка исправляет 404 на локальном сервере
+    historyApiFallback: true
+  },
   build: {
     outDir: 'dist'
   },
