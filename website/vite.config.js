f52a6ac2eb56fa4f74dcc6207c1fc79008eeb0f0
Ganjo1st <162578733+Ganjo1st@users.noreply.github.com>
2026-08-23 17:07:01 +0700

    Update vite.config.js

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,20 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/NovikonApp/',
-  server: {
-    // Эта строка исправляет 404 на локальном сервере
-    historyApiFallback: true
-  },
-  build: {
-    outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  base: '/NovikonApp/'
 })
